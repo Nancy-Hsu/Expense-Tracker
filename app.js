414600ab4b0f040b6942f7512e21f33de03ab130
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-08-29 15:16:06 +0800

    feat: initial files

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const router = require('./routes')
+app.use(router)
+
+
+
+
+app.listen(port, () => {
+  console.log(`express is listening on localhost:${port}`)
+})
